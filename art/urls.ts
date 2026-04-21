b9b83244c60eb1b6ccdff99ee4c52e2ad0efa703
Maksim Koroteev <maksimkoroteev@ttk.ru>
2026-04-21 23:49:58 +0300

    yac

--- art/urls.ts
+++ art/urls.ts
@@ -4,6 +4,6 @@ const error: Error = { page: () => import('/src/error.svelte') }
 const layout: Layout = { page: () => import('/src/base.svelte'), error: error }
 
 export const patterns: Pattern[] = [
-    {path: '', page: () => import('/src/tasks/tasks.svelte'), layouts: [layout], name: 'HOME'},
+    {path: '', page: () => import('/src/home.svelte'), layouts: [layout], name: 'HOME'},
     {path: 'test', page: () => import('/src/test.svelte'), layouts: [layout], wrapper: 'Default'},
 ]
